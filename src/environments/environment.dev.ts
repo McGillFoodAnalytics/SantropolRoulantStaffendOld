8f8523cb2bc957b782591643b9e5e6cb25864f40
Alexa Hernandez <alexa.hernandez@mail.mcgill.ca>
2019-08-31 12:53:36 -0400

    added firebase config

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,5 +5,13 @@
 
 export const AppConfig = {
   production: false,
-  environment: 'DEV'
+  environment: 'DEV',
+  firebase: {
+    apiKey: "AIzaSyBTiUXyilIjxNRhMd_DOdefWHT7-YFqfso",
+    authDomain: "santropolroulant-b4d14.firebaseapp.com",
+    databaseURL: "https://santropolroulant-b4d14.firebaseio.com",
+    projectId: "santropolroulant-b4d14",
+    storageBucket: "santropolroulant-b4d14.appspot.com",
+    messagingSenderId: "730863709360"
+  }
 };
