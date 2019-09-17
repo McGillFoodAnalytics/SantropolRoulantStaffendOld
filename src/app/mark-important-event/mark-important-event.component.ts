c8e37967f17cb578e772cd72b329b551660f43ad
Alexa Hernandez <alexa.hernandez@mail.mcgill.ca>
2019-09-16 20:55:42 -0400

    finished mark important event form

--- src/app/mark-important-event/mark-important-event.component.ts
+++ src/app/mark-important-event/mark-important-event.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mark-important-event',
@@ -12,13 +14,26 @@ export class MarkImportantEventComponent implements OnInit {
   private modalReference;
   private model: any = {};
   private form: FormGroup;
-  private event_types = ['Kitchen AM', 'Kitchen PM', 'Meal Delivery', 'Meal Delivery Driver'];
+  private eventTypes = {'Kitchen AM': 'kitam', 'Kitchen PM': 'kitpm', 'Meal Delivery': 'deliv', 'Meal Delivery Driver': 'deldr'};
+  private eventsRef: AngularFireList<any>;
+  private events: Observable<any[]>;
+  private eventDates = {};
 
-  constructor(private modalService: NgbModal, private db: AngularFireDatabase, private formBuilder: FormBuilder) {}
+
+  constructor(private modalService: NgbModal, private db: AngularFireDatabase, private formBuilder: FormBuilder) {
+    this.eventsRef = db.list('event');
+    this.events = this.eventsRef.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ id: c.payload.key, ...c.payload.val() }))
+      )
+    );
+    this.formatEventDates();
+  }
 
   ngOnInit(){
     this.form = this.formBuilder.group({
-      event_type: ['', Validators.required]
+      event_type: ['', Validators.required],
+      event_date: ['', Validators.required]
     });
   }
 
@@ -26,14 +41,66 @@ export class MarkImportantEventComponent implements OnInit {
     this.modalReference = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'sm', windowClass: 'my-class'});
   }
 
+  formatEventDates(){
+    this.events.subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+          var event_date = snapshot.event_date.toString();
+          var event_type = snapshot.event_type.toString();
+          event_date = this.formatDate(event_date);
+          if (!(event_date in this.eventDates)){
+            this.eventDates[event_date] = {};
+            this.eventDates[event_date][event_type] = [snapshot.id];
+          }
+          else{
+            if (!(event_type in this.eventDates[event_date])){
+              this.eventDates[event_date][event_type] = [snapshot.id];
+            }
+            else{
+              this.eventDates[event_date][event_type].push(snapshot.id);
+            }
+          }
+        });
+    });
+  }
+
+  formatDate(date: string){
+    const year = "20" + date.substring(0,2);
+    const month = date.substring(2,4);
+    const day = date.substring(4,6);
+    date = month+'/'+day+'/'+year;
+    return date;
+  }
+
+  formatEventType(event_type: string){
+
+  }
+
   onSubmit(){
     this.form.markAllAsTouched();
     if (this.form.valid) {
-      console.log(this.model);
       this.modalReference.close();
+      const event_date = this.model.event_date;
+      const event_type = this.model.event_type;
+      this.markImportantEvents(event_date, event_type);
       this.form.reset();
       this.model = {};
     }
+  }
+
+  markImportantEvents(event_date: string, event_type: string){
+    var event_type = this.eventTypes[event_type];
+    var important_events = this.eventDates[event_date][event_type];
+    for (var event of important_events){
+      this.markEventAsImportant(event);
+    }
+  }
+
+  markEventAsImportant(event_id: string){
+    this.db.object('/event/' + event_id).update(
+      {
+        is_important_event: true
+      }
+    );
   }
 
 }
