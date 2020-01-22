7c2a75a695594e42ac0f499863a278574bdec425
Yousuf Badawi <43317879+yousufBadawi@users.noreply.github.com>
2020-01-21 20:22:44 -0500

    what

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -230,7 +230,7 @@ export class UserListComponent {
   {
     this.firebase.removeUserFromEvent(event_id);
     this.currentEventsUser = [];
-    this.displayCurrentEvents(this.volunteer.first_name, volunteer.last_name);
+    this.displayCurrentEvents(this.volunteer.first_name, this.volunteer.last_name);
   }
 
   updateUser(firstName, lastName, email){
