3927b88072c8a6265206b83eb1e4f784a568864c
unknown <parenty.jean@gmail.com>
2020-04-19 19:46:23 -0400

    add expandable row to volunteer directory table

--- src/app/volunteer-directory/volunteer-directory.component.ts
+++ src/app/volunteer-directory/volunteer-directory.component.ts
@@ -4,18 +4,29 @@ import {FireBaseService} from '../core/firebaseService';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { User } from '../shared/models/user';
+
 
 @Component({
   selector: 'app-volunteer-directory',
   templateUrl: './volunteer-directory.component.html',
   styleUrls: ['./volunteer-directory.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 
 export class VolunteerDirectoryComponent implements OnInit {
-  private displayedColumns: string[] = ['id', 'first_name', 'last_name'];
+  private displayedColumns: string[] = [ 'first_name', 'last_name', 'email', 'phone_number'];
   private volunteers: any = [];
   private volunteersObservable;
+  private expandableColumns
   dataSource;
+  expandedElement: User;
 
 
   constructor(private fs: FireBaseService) { }
@@ -29,9 +40,9 @@ export class VolunteerDirectoryComponent implements OnInit {
     this.dataSource = new MatTableDataSource(this.volunteers);
     let temp = Object.keys(this.volunteers[0]);
     temp = temp.filter(e => !this.displayedColumns.includes(e));
-    this.displayedColumns = this.displayedColumns.concat(temp);
+    //this.displayedColumns = this.displayedColumns.concat(temp);
   });
-}
+  }
 
   prettify(str: string) {
     return str.replace('_', ' ');
@@ -41,5 +52,6 @@ export class VolunteerDirectoryComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  //missing the deleteUser() method 
 
 }
